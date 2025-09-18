247ca369b76211fc4ff1f56abd68fbb91b3361c8
Olufayo Emmanuel <olufalzy01@gmail.com>
2025-09-18 22:03:29 +1300

    vercel patch

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,26 @@
+// // import { defineConfig } from 'vite'
+// // import react from '@vitejs/plugin-react'
+
+// // // https://vite.dev/config/
+// // export default defineConfig({
+// //   plugins: [react()],
+// //   base: '/Unique-Truth-Auto-Limited/',
+// // })
+
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
 
 // // https://vite.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
-//   base: '/Unique-Truth-Auto-Limited/',
+//   base: '/',
 // })
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
+  base: "/", // ✅ Correct for Vercel
   plugins: [react()],
-  base: '/',
-})
+});
